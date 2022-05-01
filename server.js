55521013498a204a1a4729c47a6a5de684b6139b
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-30 20:33:18 -0400

    Add PORT const

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { animals } = require('./data/animals')
+const { animals } = require('./data/animals');
+
+const PORT = process.env.PORT || 3001;
 
 // to instantiate the server
 const app = express();
@@ -54,6 +56,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 // to make our server listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 })
